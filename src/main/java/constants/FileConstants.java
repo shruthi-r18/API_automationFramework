3d3f3b542bebe5b6a26f463aca49fb854aec30c9
shruthi-r18 <shruthiramachandra162@gmail.com>
2023-05-07 02:12:40 -0700

    first commit

---
+++ src/main/java/constants/FileConstants.java
@@ -0,0 +1,20 @@
+package constants;
+
+/**
+ * @author shrut
+ *
+ */
+public class FileConstants {
+	
+	public static final String ENVI_URI_FILE_PATH = System.getProperty("user.dir")+"\\src\\main\\resources\\config\\envURIs.json";
+	public static final String USER_ACCOUNTS_FILE_PATH = System.getProperty("user.dir")+"\\src\\main\\resources\\config\\useraccounts.json";
+	public static final String ADD_USER_DATA_FILE_PATH = System.getProperty("user.dir")+"\\src\\main\\resources\\payload\\adduserdata.json";
+	public static final String UPDATE_USER_DATA_FILE_PATH = System.getProperty("user.dir")+"\\src\\main\\resources\\payload\\updateuserdata.json";
+	public static final String DELETE_USER_DATA_FILE_PATH = System.getProperty("user.dir")+"\\src\\main\\resources\\payload\\deleteuserdata.json";
+	public static final String ADD_DATA_VALIDATION_FILE_PATH = System.getProperty("user.dir")+"\\src\\main\\resources\\testdatavalidation\\adddatavalidation.json";
+	public static final String DELETE_DATA_VALIDATION_FILE_PATH = System.getProperty("user.dir")+"\\src\\main\\resources\\testdatavalidation\\deletedatavalidation.json";
+	public static final String LOGIN_VALIDATION_FILE_PATH = System.getProperty("user.dir")+"\\src\\main\\resources\\testdatavalidation\\loginvalidation.json";
+	public static final String  UPDATE_DATA_VALIDATION_FILE_PATH= System.getProperty("user.dir")+"\\src\\main\\resources\\testdatavalidation\\updatedatavalidation.json";
+	public static final String LOGIN_SCHEMA_FILE_PATH= System.getProperty("user.dir")+"\\src\\main\\java\\schema\\LoginResponseSchema.json";
+
+}
